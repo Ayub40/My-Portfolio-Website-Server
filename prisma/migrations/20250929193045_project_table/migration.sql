69a73b2c98f45653b90e5f59a76df08abdd71355
Ayub40 <ayubk4028@gmail.com>
2025-09-30 01:31:20 +0600

    schema.prisma added

---
+++ prisma/migrations/20250929193045_project_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "public"."Project" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "thumbnail" TEXT,
+    "liveUrl" TEXT,
+    "githubUrl" TEXT,
+    "features" TEXT[],
+    "ownerId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Project" ADD CONSTRAINT "Project_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
